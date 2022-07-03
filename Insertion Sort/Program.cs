21cbacf4629df05962d5292b7e237149fb562d42
ThiagoWlian <52060131+ThiagoWlian@users.noreply.github.com>
2022-07-03 05:26:12 -0300

    Update Program.cs

--- Insertion Sort/Program.cs
+++ Insertion Sort/Program.cs
@@ -1,24 +1,9 @@
-﻿int[] vetorDesordenado = { 7, 9, 4, 40, 6 };
-
-
-int encontraIndiceMenorNumero(int inicioIndice)
-{
-    int indiceMenorNumero = inicioIndice;
-    for (int indice = inicioIndice; indice < vetorDesordenado.Length; indice++)
-    {
-        if (vetorDesordenado[indice] < vetorDesordenado[indiceMenorNumero])
-        {
-            indiceMenorNumero = indice;
-        }
-    }
-    return indiceMenorNumero;
-}
+int[] vetorDesordenado = {7,9,4,40,6};
 
 void ordenaVetorNumerico()
 {
-    for (int indice = 0; indice < vetorDesordenado.Length; indice++)
+    for(int indice = 0; indice < vetorDesordenado.Length; indice++)
     {
-        int indiceMenorNumero = encontraIndiceMenorNumero(indice);
         int atualValor = vetorDesordenado[indice];
         int maiorValor = vetorDesordenado[indiceMenorNumero];
 
